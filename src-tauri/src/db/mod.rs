0f990d6bab3e9c62ae30ea0c9e592365568eb354
bryanz35 <78311442+bryanz35@users.noreply.github.com>
2026-08-27 12:00:54 -0400

    db reading

--- src-tauri/src/db/mod.rs
+++ src-tauri/src/db/mod.rs
@@ -1,3 +1,4 @@
+use crate::error::Result;
 use rusqlite::Connection;
 use std::path::Path;
 use std::sync::Mutex;
@@ -5,10 +6,17 @@ use std::sync::Mutex;
 pub mod migrations;
 
 pub struct Db(pub Mutex<Connection>);
-
-pub fn open(dir: &Path) -> crate::Result<Connection> {
+// creates db files and opens connection, then moves handling to migrations.rs
+pub fn open(dir: &Path) -> Result<Connection> {
     std::fs::create_dir_all(dir)?;
     let conn = Connection::open(dir.join("doit.sqlite3"))?;
 
+    // what?
+    conn.pragma_update(None, "foreign_keys", "ON")?;
+
+    conn.pragma_update(None, "journal_mode", "WAL")?;
+
+    conn.busy_timeout(std::time::Duration::from_secs(5))?;
+    migrations::run(&conn)?;
     Ok(conn)
 }
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,17 +1,22 @@
 mod db;
 mod error;
 mod model;
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+
+pub use error::{AppError, Result};
+
+use tauri::Manager;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            let dir = app.path().app_data_dir()?;
+            let conn = db::open(&dir)?;
+            app.manage(db::Db(std::sync::Mutex::new(conn)));
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .expect("Error while running tauri application")
 }
