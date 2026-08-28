fd7b36e3d549bb505dc55e6e034d99e32d319b58
bryanz35 <78311442+bryanz35@users.noreply.github.com>
2026-08-27 22:19:36 -0400

    Update model.rs

--- src-tauri/src/model.rs
+++ src-tauri/src/model.rs
@@ -1,1 +1,37 @@
 // Data types go here; will mirror src/types.ts
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "kebab-case")] // why kebab case?
+pub enum TaskStatus {
+    Todo,
+    InProgress,
+    Done,
+    Idea,
+    Blocked,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Task {
+    pub id: String,
+    pub title: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub notes: Option<String>,
+    pub status: TaskStatus,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub due: Option<String>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub estimate_minutes: Option<i64>, //estimateMinutes in ts
+
+    pub tags: Vec<String>,
+    pub depends_on: Vec<String>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct TaskPatch {
+    pub title: Option<String>,
+    pub notes: Option<String>,
+    pub status: Option<TaskStatus>,
+}
